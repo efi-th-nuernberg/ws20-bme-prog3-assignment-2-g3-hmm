ea27d2fb454e34870041fa0b8ce61bf351715a8b
repl.it user <replituser@example.com>
2020-11-08 17:06:52 +0000

    TF new

--- TextFormatter.java
+++ TextFormatter.java
@@ -1,4 +1,3 @@
-
 class TextFormatter {
 private int maxLength;
 
@@ -16,8 +15,11 @@ private int maxLength;
   public static void main(String[] args) {
 
     TextFormatter formatter = new TextFormatter(30); 
-    formatter.print(text);
+  //  formatter.printLeft(text);
+    formatter.printRight(text);
+
   }
+
 
   // Konstruktor
   public TextFormatter(int maxLineLength) {
@@ -25,21 +27,46 @@ private int maxLength;
   }
   
   // Ausgabe
-  public void print(String aText) {
+  public void printLeft(String aText) {
     String[] words = aText.split(" ");
     int letterCounter = 0;
 	  for(int i = 0; i < words.length; i++) {
       int wordLength = words[i].length();
-	    if(letterCounter + wordLength <= maxLength) {
+	    if (letterCounter + wordLength <= maxLength) {
 	      letterCounter += wordLength + 1;
-	    	System.out.print(words[i] + " ");
+        System.out.print(words[i] + " ");
 	    }else{
 	    	System.out.print("\n" + words[i] + " ");
         letterCounter = wordLength + 1;
       }
     }
-      System.out.println();
+    System.out.println();
+  }
+
+  public void printRight(String aText) {
+    String[] words = aText.split(" ");
+    String line = "";
+    int letterCounter = 0;
+	  for(int i = 0; i < words.length; i++) {
+      int wordLength = words[i].length();
+	    if(letterCounter + wordLength <= maxLength) {
+	      letterCounter += wordLength + 1;
+        line += " " + words[i];
+	    }else{
+       
+        while(line.length() < maxLength){
+          line = " " + line;
+        }
+    
+	    	System.out.print( line + "\n");
+        letterCounter = wordLength + 1;
+        line = " " + words[i];
+      }
+    }
+     while(line.length() < maxLength){
+          line = " " + line;
+        }
+    System.out.println(line);
   }
 } 	
-
 
